0b097add63e0ec8ba2b66c00b4204b4c942b6004
retsa-exe <asterlin7890@gmail.com>
2025-09-26 17:41:52 -0400

    Finished Week 4 Experiment

---
+++ Assets/Scripts/Alien.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Alien : MonoBehaviour
+{
+    public Vector3 hook;
+    public float hookSpeed = 2f;
+    public float hookLength = 3f;
+    public float maxAngle = 60f;
+
+    public float shootDuration = 2f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //reset the hook position
+        hook = transform.position + Vector3.up * hookLength;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HookSwing();
+    }
+
+    public void HookSwing()
+    {
+        float angle = Mathf.Sin(Time.time * hookSpeed) * maxAngle + 90f;
+        float radius = angle * Mathf.Deg2Rad;
+
+        float hookX = Mathf.Cos(radius);
+        float hookY = Mathf.Sin(radius);
+
+        hook = transform.position + new Vector3(hookX, hookY, 0) * hookLength;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+
+        Debug.DrawLine(transform.position, hook, Color.green);
+    }
+}
