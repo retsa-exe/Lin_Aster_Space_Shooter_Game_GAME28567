2cb12280cec686186472e98e7dfbc178d7e5f333
Aster Lin <asterlin7890@gmail.com>
2025-10-09 22:23:15 -0400

    Add hook shooting mechanic

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -6,16 +6,26 @@ public class Alien : MonoBehaviour
     public float hookSpeed = 2f;
     public float hookLength = 3f;
     public float maxAngle = 60f;
+    Vector3 direction;
 
     public float shootDuration = 2f;
 
     public GameObject hookPrefab;
+
+    public bool isShooting = false;
+    float t;
+    float shootTimer = 0f;
+    public float shootSpeed = 2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //reset the hook position
         hook = transform.position + Vector3.up * hookLength;
+
+        //reset the timer
+        t = 0f;
+        shootTimer = 0f;
     }
 
     // Update is called once per frame
@@ -26,17 +36,48 @@ public class Alien : MonoBehaviour
 
     public void HookSwing()
     {
-        float angle = Mathf.Sin(Time.time * hookSpeed) * maxAngle + 90f;
-        float radius = angle * Mathf.Deg2Rad;
-
-        float hookX = Mathf.Cos(radius);
-        float hookY = Mathf.Sin(radius);
-
-        hook = transform.position + new Vector3(hookX, hookY, 0) * hookLength;
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        if (Input.GetKeyDown(KeyCode.Space) && !isShooting)
         {
+            isShooting = true;
 
+            float angle = Mathf.Sin(t * hookSpeed) * maxAngle + 90f;
+            float radius = angle * Mathf.Deg2Rad;
+
+            float hookX = Mathf.Cos(radius);
+            float hookY = Mathf.Sin(radius);
+
+            direction = new Vector3(hookX, hookY, 0);
+        }
+
+        if (!isShooting)
+        {
+            //update the timer
+            t += Time.deltaTime;
+
+            float angle = Mathf.Sin(t * hookSpeed) * maxAngle + 90f;
+            float radius = angle * Mathf.Deg2Rad;
+
+            float hookX = Mathf.Cos(radius);
+            float hookY = Mathf.Sin(radius);
+
+            direction = new Vector3(hookX, hookY, 0);
+
+            hook = transform.position + direction * hookLength;
+        }
+
+        if (isShooting)
+        {
+            //update shoot timer
+            shootTimer += Time.deltaTime;
+
+            hook = transform.position + direction * hookLength * (1 + shootTimer * shootSpeed);
+
+            if ( shootTimer > shootDuration)
+            {
+                isShooting = false;
+                shootTimer = 0f;
+            }
         }
 
         Debug.DrawLine(transform.position, hook, Color.yellow);
@@ -45,7 +86,6 @@ public class Alien : MonoBehaviour
         hookPrefab.transform.position = hook;
 
         //set hook rotation
-        Vector3 direction = hook - transform.position;
         float angleDegree = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         hookPrefab.transform.rotation = Quaternion.Euler(0, 0, angleDegree + 90f);
     }
